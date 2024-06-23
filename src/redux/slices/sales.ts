b5d1cdc26e018b4e754a4d4d880cd7f7302f260e
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-06-23 13:19:59 +0300

    almost

--- src/redux/slices/sales.ts
+++ src/redux/slices/sales.ts
@@ -1,27 +1,22 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getSalesData } from "../../api/getSalesData";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {getSalesData} from "../../api/getSalesData";
 
 export interface IStateSales {
-    data: { [year: string]: number };
+    data: any;
 }
 
 const initialState: IStateSales = {
-    data: {},
+    data: [],
 };
 
 export const getData = createAsyncThunk(
     "sales/getData",
     async () => {
-        const response = await getSalesData();
-        const yearData = response.year;
-        const salesByYear: { [year: string]: number } = {};
-
-        // Преобразуем объект year с годами и продажами в нужный формат
-        Object.keys(yearData).forEach((year) => {
-            salesByYear[year] = parseInt(yearData[year], 10);
-        });
-
-        return salesByYear;
+        try {
+            return await getSalesData();
+        } catch (error) {
+            throw Error("Failed to fetch sales data");
+        }
     }
 );
 
@@ -33,7 +28,7 @@ export const salesSlice = createSlice({
         builder
             .addCase(getData.fulfilled, (state, action) => {
                 state.data = action.payload;
-            });
+            })
     },
 });
 
