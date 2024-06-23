6d1eccedd5a688ad0854a1708d80ee18dcfc8878
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-06-23 14:08:21 +0300

    almost

--- src/api/getSalesData.ts
+++ src/api/getSalesData.ts
@@ -2,6 +2,5 @@ import {instance} from "./instance";
 
 export const getSalesData = async () => {
         const {data} = await instance.get("/data");
-        console.log(data)
         return data;
 }
